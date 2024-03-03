9e54cb829dc90e32b2b843d93fd007ba9539882b
dannydollars <danielcmatheson@gmail.com>
2024-03-02 21:00:40 -0400

    add user and pass

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 
 
 const app = express();
 
 app.get('/email', (req, res) => {
-    res.send('Hello World');
+
 });
+
+app.post ('/email', (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASS
+        }
+    });
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('App is running');
 });
+
