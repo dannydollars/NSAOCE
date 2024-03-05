5757111f0d1c2ee0827f60555b55ae2521cf05ca
dannydollars <danielcmatheson@gmail.com>
2024-03-04 21:46:33 -0400

    rate limit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const multer = require('multer');
 const cors = require('cors');
+const rateLimit  =  require('express-rate-limit');
+
 const transporter = nodemailer.createTransport({
     host: 'mail.pathfinder-group.com',
     port: 465,
@@ -12,8 +14,15 @@ const transporter = nodemailer.createTransport({
         pass: process.env.PASS
     }
 });
-
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 40 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  });
+  
+  //  apply to all requests
+  app.use(limiter);
 app.use(cors());
 
 app.get('/email', (req, res) => {
