48d5a97f6521d2ffe61bf211496ef8b88f7e03bc
dannydollars <danielcmatheson@gmail.com>
2024-03-04 21:20:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const multer = require('multer');
-
+const cors = require('cors');
 const transporter = nodemailer.createTransport({
     host: 'mail.pathfinder-group.com',
     port: 465,
@@ -14,6 +14,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const app = express();
+app.use(cors());
 
 app.get('/email', (req, res) => {
 console.log("hi")
@@ -23,12 +24,12 @@ const upload = multer();
 
 app.post('/send-email', upload.none(), (req, res) => {
    res.send('Form data received!');
-
+console.log(req.body)
    transporter.sendMail({
         from: process.env.USER,
     to:'danielcmatheson@gmail.com',
-    subject: "contact form submission",
-    text: req.body.Message,
+    subject: "contact form submission from "+req.body.name,
+    text: req.body.message,
     replyTo:req.body.email
     })
 })
